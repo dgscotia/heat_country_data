609387849c35e6a734aba3ebdfd4ae70e53ce5fc
duncan <duncanmgibb@gmail.com>
2022-02-18 20:56:18 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+1. Import the datasets
+2. Put them into a dataframe
+3. Make the pie charts with seaborn
+
+
+"""
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
