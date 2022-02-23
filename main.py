d7463fbf0b64fc77e6d093aff65e62439da1656e
duncan <duncanmgibb@gmail.com>
2022-02-23 14:06:09 +0100

    updated styles

--- main.py
+++ main.py
@@ -9,3 +9,11 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+df_1 = pd.read_csv("heat_data_fig_1_python.csv").set_index("Country")
+df_2 = pd.read_csv("heat_data_fig_2_python.csv").set_index("Country")
+fontprops = {'fontsize': 25, 'fontname': 'Microsoft Yi Baiti'}
+colors = sns.color_palette("tab20")
+
+labels_1 = df_1.columns
+labels_2 = df_2.columns
